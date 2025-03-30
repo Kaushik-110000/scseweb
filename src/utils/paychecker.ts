2f9ce1abba73f076bc0a2ab0eb2a6468b53244b9
Kaushik-110000 <abhishekkumar89647@gmail.com>
2025-03-30 16:07:18 +0530

    Update paychecker.ts
    const isFromCse =
      /^[0-9]{4}(?:ugcs|pgcsca|rscs|rsca)[0-9]{3}@nitjsr\.ac\.in$|^[0-9]{4}(?:pgcscs|pgcsds|pgcsis)[0-9]{2}@nitjsr\.ac\.in$/i.test(
        email
      );

--- src/utils/paychecker.ts
+++ src/utils/paychecker.ts
@@ -1,8 +1,8 @@
 export const checkIsFromCse = (email: string) => {
   const isFromCse =
-    /^[0-9]{4}(?:ugcs|pgcsca|pgcscs|pgcsds|pgcsis)[0-9]{3}@nitjsr\.ac\.in$/i.test(
-      email
-    ); //lets see
+  /^[0-9]{4}(?:ugcs|pgcsca|rscs|rsca)[0-9]{3}@nitjsr\.ac\.in$|^[0-9]{4}(?:pgcscs|pgcsds|pgcsis)[0-9]{2}@nitjsr\.ac\.in$/i.test(
+    email
+  ); //lets see
   console.log("hi");
   return isFromCse;
 };
