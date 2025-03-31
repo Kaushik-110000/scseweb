da752de31fb4e6b2a0b18f8dd2e7739ff4133e9b
riteshjha8 <riteshjha689@gmail.com>
2025-04-01 01:18:33 +0545

    Update epage.tsx

--- src/app/events/epage.tsx
+++ src/app/events/epage.tsx
@@ -230,7 +230,7 @@ export default function Page() {
         more: "Bring your own laptops",
         rules: "Solo participation event",
         minPart: 1,
-        maxPart: 3,
+        maxPart: 1,
       },
       {
         _id: "67e1b591b1e72e5f2f25d73f",
